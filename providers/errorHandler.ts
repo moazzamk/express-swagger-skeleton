d386269d6336df31a906c5c3f973edc072367fd9
moz <moazzamk@gmail.com>
2022-09-28 10:35:39 -0500

    Error responses now have correct response code

--- providers/errorHandler.ts
+++ providers/errorHandler.ts
@@ -2,6 +2,7 @@
 export const jsonErrorHandler = (err, req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   console.log(err);
+  res.status(err.status);
   res.send({
     status: err.status,
     message: err.message
